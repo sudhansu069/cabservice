66a3e6e45ad1776e7255b2b7dc6a6535f4b11547
Sudhansu Sekhar Jena <sudhansu.jena069@gmail.com>
2018-01-10 20:31:05 +0530

    Add files via upload

---
+++ src/main/java/com/target/cabservice/dto/CabRequestWrapper.java
@@ -0,0 +1,20 @@
+package com.target.cabservice.dto;
+
+import java.util.List;
+
+public class CabRequestWrapper {
+
+	private List<CabDTO> cabs ;
+
+	public List<CabDTO> getCabs() {
+		return cabs;
+	}
+
+	public void setCabs(List<CabDTO> cabs) {
+		this.cabs = cabs;
+	}
+
+	 
+	
+	
+}
---
+++ src/main/java/com/target/cabservice/dto/RegistrationDTOWrapper.java
@@ -0,0 +1,20 @@
+package com.target.cabservice.dto;
+
+import java.util.List;
+
+public class RegistrationDTOWrapper {
+  
+	List<RegistrationDTO> team_members;
+
+	public List<RegistrationDTO> getTeam_members() {
+		return team_members;
+	}
+
+	public void setTeam_members(List<RegistrationDTO> team_members) {
+		this.team_members = team_members;
+	}
+
+	 
+	
+	
+}
